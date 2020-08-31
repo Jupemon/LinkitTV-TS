e207c4df31c9c9ae4f0b3a8198d502f7e50707bf
Juho <s6heju04@gmail.com>
2020-08-31 19:14:26 +0300

    built the landing page

--- Components/LandingPage.tsx
+++ Components/LandingPage.tsx
@@ -6,7 +6,7 @@ const LandingPage = () => {
         <p>Watch youtube videos suggested by your viewers</p>
         <img src="/logo.png" alt="logo" width="90%"/>
         <Link href="/session">
-            <div onMouseEnter={(e) => {e.currentTarget.style.backgroundColor = "white"; e.currentTarget.style.color = "purple"}} onMouseOut={(e) => {e.currentTarget.style.backgroundColor = "purple"; e.currentTarget.style.color = "white"}} style={{width : "70px", marginRight :"50%", marginLeft:"50%", cursor : "pointer"}}>Start</div>
+            <div className="Button">Start</div>
         </Link>
     </div> );
 }
